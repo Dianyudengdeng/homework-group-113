b112740476a017df1b97d36c10e5d3511b0c50fb
Dianyudengdeng <93588357+Dianyudengdeng@users.noreply.github.com>
2023-07-27 01:04:10 +0800

    Add files via upload

---
+++ Project11/SM2 Signature.py
@@ -0,0 +1,57 @@
+from gmssl import sm3, func
+from tools import *
+from Generate_K import *
+
+def precompute(ID, a, b, G_X, G_Y, x_A, y_A):
+    ENTL = str(get_bit_num(ID))
+    joint = ENTL+ID+str(a)+str(b)+str(G_X)+str(G_Y)+str(x_A)+str(y_A)
+    joint_b = bytes(joint, encoding='utf-8')
+    digest = sm3.sm3_hash(func.bytes_to_list(joint_b))
+    return int(digest, 16)
+
+def sign(private_key, message, Z_A):
+    M_1 = str(Z_A) + message
+    M_b = bytes(M_1, encoding='utf-8')
+    e = sm3.sm3_hash(func.bytes_to_list(M_b))
+    e = int(e, 16)
+
+    k = generate_k(message, private_key, N, get_bit_num(N), hash_function=hashlib.sha256)
+    kG = point_multiply(k, G)
+
+    r = (e + kG[0]) % N
+    s = (mod_inverse(1 + private_key, N) * (k - r * private_key)) % N
+    print(f"{message} 生成签名对:")
+    print(f"r:{r}\ns:{s}")
+    return (r, s)
+
+
+def verify(public_key, ID, message, signature):
+    r = signature[0]
+    s = signature[1]
+
+    Z_A  = precompute(ID, A, B, G_X, G_Y, public_key[0], public_key[1])
+
+    M_1 = str(Z_A) + message
+    M_b = bytes(M_1, encoding='utf-8')
+    e = sm3.sm3_hash(func.bytes_to_list(M_b))  # str
+    e = int(e, 16)
+    t = (r + s) % N
+
+    result = point_add(point_multiply(s, G), point_multiply(t, public_key))
+
+    x1 = result[0]
+    x2 = result[1]
+    R = (e + x1) % N
+
+    return R == r
+
+if __name__=="__main__":
+    ID = "202100460030"
+    message="我是待签名的消息"
+    sk, pk = generate_key()
+    Z_A=precompute(ID, A, B, G_X, G_Y, pk[0], pk[1])
+    signature = sign(sk, message, Z_A)
+    if verify(pk, ID, message, signature):
+        print("Verification passed")
+    else:
+        print("Verification failed")
---
+++ Project11/tools.py
@@ -0,0 +1,143 @@
+import secrets
+
+#y^2=x^3+7
+'''使用secp256k1曲线参数
+P：secp256k1曲线定义的有限域（或称为素数域）的模数
+N：曲线上的基点 G 的阶数
+G_X G_Y:基点 G 的横坐标和纵坐标
+'''
+A = 0
+B = 7
+P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+G_X = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+G_Y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+G = (G_X, G_Y)
+
+'''
+extended_euclidean_algorithm()扩展欧几里得算法<br>
+mod_inverse()模逆运算<br>
+elliptic_add()椭圆曲线加<br>
+elliptic_double()椭圆曲线自加<br>
+elliptic_multiply()椭圆曲线倍点运算<br>
+generate_key()生成公私钥对<br>
+sign()实现sm2签名<br>
+verify()实现sm2验证签名<br>
+'''
+
+def get_bit_num(x):
+    if isinstance(x, int):
+        num = 0
+        tmp = x >> 64
+        while tmp:
+            num += 64
+            tmp >>= 64
+        tmp = x >> num >> 8
+        while tmp:
+            num += 8
+            tmp >>= 8
+        x >>= num
+        while x:
+            num += 1
+            x >>= 1
+        return num
+    elif isinstance(x, str):
+        return len(x.encode()) << 3
+    elif isinstance(x, bytes):
+        return len(x) << 3
+    return 0
+
+
+def extended_euclidean(j, k):
+    if j == k:
+        return (j, 1, 0)
+    else:
+        i = 0
+        j_array = [j]
+        k_array = [k]
+        q_array = []
+        r_array = []
+
+        prev_r_is_zero = False
+
+        while not (prev_r_is_zero):
+            q_array.append(k_array[i]//j_array[i])
+            r_array.append(k_array[i]%j_array[i])
+            k_array.append(j_array[i])
+            j_array.append(r_array[i])
+            i += 1
+            if r_array[i-1] == 0:
+                prev_r_is_zero = True
+        i -= 1
+        gcd = j_array[i]
+        x_array = [1]
+        y_array = [0]
+
+        i -= 1
+        total_steps = i
+
+        while i >= 0:
+            y_array.append(x_array[total_steps-i])
+            x_array.append(y_array[total_steps-i] - q_array[i]*x_array[total_steps-i])
+            i -= 1
+
+        return (gcd, x_array[-1], y_array[-1])
+
+def mod_inverse(j, n):
+    (gcd, x, y) = extended_euclidean(j, n)
+
+    if gcd == 1:
+        return x % n
+    else:
+        return -1
+
+
+def point_add (p, q):
+    if p == 0 and q == 0: return 0
+    elif p == 0: return q
+    elif q == 0: return p
+    else:
+        # Swap p and q if px > qx.
+        if p[0] > q[0]:
+            temp = p
+            p = q
+            q = temp
+        r = []
+        slope = (q[1] - p[1])*mod_inverse(q[0] - p[0], P) % P
+
+        r.append((slope**2 - p[0] - q[0]) % P)
+        r.append((slope*(p[0] - r[0]) - p[1]) % P)
+
+        return (r[0], r[1])
+
+
+
+def point_double(p):
+    r = []
+
+    slope = (3*p[0]**2 + A)*mod_inverse(2*p[1], P) % P
+
+    r.append((slope**2 - 2*p[0])%P)
+    r.append((slope*(p[0] - r[0]) - p[1])%P)
+
+    return (r[0], r[1])
+
+def point_multiply(s, p):
+    n = p
+    r = 0 # 0 representing a point at infinity
+
+    s_binary = bin(s)[2:]
+    s_length = len(s_binary)
+
+    for i in reversed(range(s_length)):
+        if s_binary[i] == '1':
+            r = point_add(r, n)
+        n = point_double(n)
+
+    return r
+
+
+def generate_key():
+    private_key = int(secrets.token_hex(32), 16)#随机产生成私钥
+    public_key = point_multiply(private_key, G)#计算得到公钥
+    return private_key, public_key
